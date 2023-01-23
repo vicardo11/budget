586bcd544d2715b3967c8e3774d946431738072c
vicardo11 <sosiniak95@gmail.com>
2023-01-23 19:17:28 +0100

    AccountBalance - initial commit

---
+++ account-balance-service/src/main/java/it/sosinski/accountbalance/AccountBalanceApplication.java
@@ -0,0 +1,13 @@
+package it.sosinski.accountbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountBalanceApplication.class, args);
+    }
+
+}
---
+++ account-balance-service/src/test/java/it/sosinski/accountbalance/AccountBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sosinski.accountbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
