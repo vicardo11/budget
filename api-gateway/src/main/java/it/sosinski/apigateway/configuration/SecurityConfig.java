32e7ca4bcfa568ad03cc7d905108cda5b809afc7
vicardo11 <sosiniak95@gmail.com>
2023-01-23 17:18:06 +0100

    Gateway - authorization

---
+++ api-gateway/src/main/java/it/sosinski/apigateway/configuration/SecurityConfig.java
@@ -0,0 +1,21 @@
+package it.sosinski.apigateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
+        return serverHttpSecurity.build();
+    }
+}
