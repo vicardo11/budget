93e80aa78558054532ab6e1c98f8ac7fa56b797f
vicardo11 <sosiniak95@gmail.com>
2023-01-25 12:50:26 +0100

    Gateway - add routing to expenses

---
+++ api-gateway/src/main/java/it/sosinski/apigateway/configuration/RoutesConfig.java
@@ -0,0 +1,20 @@
+package it.sosinski.apigateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutesConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/expenses/**")
+                        .uri("http://localhost:8200"))
+                .build();
+    }
+
+}
