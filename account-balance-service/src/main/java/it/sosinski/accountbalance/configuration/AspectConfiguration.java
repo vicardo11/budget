7b18704e0236294f0625cda626bacf12ba782ef6
vicardo11 <sosiniak95@gmail.com>
2023-01-25 19:33:48 +0100

    AccountBalance - configuration for aspects

---
+++ account-balance-service/src/main/java/it/sosinski/accountbalance/configuration/AspectConfiguration.java
@@ -0,0 +1,14 @@
+package it.sosinski.accountbalance.configuration;
+
+import it.sosinski.aspectdirectory.logger.MethodAroundAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AspectConfiguration {
+
+    @Bean
+    public MethodAroundAspect methodEntryAspect() {
+        return new MethodAroundAspect();
+    }
+}
