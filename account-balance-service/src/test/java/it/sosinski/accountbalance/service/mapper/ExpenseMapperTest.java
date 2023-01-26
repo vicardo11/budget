0fbc88f565116f8ff94cabc55e4d4fa2f4f3e38b
vicardo11 <sosiniak95@gmail.com>
2023-01-26 09:12:01 +0100

    AccountBalance - ExpenseMapperTest refactor

--- account-balance-service/src/test/java/it/sosinski/accountbalance/service/mapper/ExpenseMapperTest.java
+++ account-balance-service/src/test/java/it/sosinski/accountbalance/service/mapper/ExpenseMapperTest.java
@@ -12,52 +12,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ExpenseMapperTest {
 
+    private final ExpenseMapper expenseMapper = Mappers.getMapper( ExpenseMapper.class);
     private static final BigDecimal VALUE_150 = BigDecimal.valueOf(150);
     private static final LocalDateTime DATE_TIME_2017 = LocalDateTime.of(2017, 1, 1, 10, 10);
     private static final String TITLE_CAR = "Car";
-    private final ExpenseMapper expenseMapper = Mappers.getMapper( ExpenseMapper.class);
 
     @Test
-    void shouldMapValue() {
+    void expenseToExpenseResponseDtoShouldMapValues() {
         // Given
-        Expense expense = Expense.builder()
+        Expense expense = expense();
+
+        // When
+        ExpenseResponseDto expenseResponseDto = expenseMapper.toResponseDto(expense);
+
+        // Then
+        assertAll(
+                () -> assertEquals(VALUE_150, expenseResponseDto.getValue()),
+                () -> assertEquals(DATE_TIME_2017, expenseResponseDto.getDateTime()),
+                () -> assertEquals(TITLE_CAR, expenseResponseDto.getTitle())
+        );
+    }
+
+    private static Expense expense() {
+        return Expense.builder()
                 .value(VALUE_150)
-                .build();
-
-        // When
-        ExpenseResponseDto expenseResponseDto = expenseMapper.toResponseDto(expense);
-
-        // Then
-        assertEquals(VALUE_150, expenseResponseDto.getValue());
-    }
-
-    @Test
-    void shouldMapDateTime() {
-        // Given
-        Expense expense = Expense.builder()
                 .dateTime(DATE_TIME_2017)
-                .build();
-
-        // When
-        ExpenseResponseDto expenseResponseDto = expenseMapper.toResponseDto(expense);
-
-        // Then
-        assertEquals(DATE_TIME_2017, expenseResponseDto.getDateTime());
-    }
-
-    @Test
-    void shouldMapTitle() {
-        // Given
-        Expense expense = Expense.builder()
                 .title(TITLE_CAR)
                 .build();
-
-        // When
-        ExpenseResponseDto expenseResponseDto = expenseMapper.toResponseDto(expense);
-
-        // Then
-        assertEquals(TITLE_CAR, expenseResponseDto.getTitle());
     }
-
-
 }
