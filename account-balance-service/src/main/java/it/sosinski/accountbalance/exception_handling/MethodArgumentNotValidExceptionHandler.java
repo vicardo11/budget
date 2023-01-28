d48abf8c38d23bc7a0211dd11a6b3e1679e972c6
vicardo11 <sosiniak95@gmail.com>
2023-01-28 10:06:24 +0100

    AccountBalance - add MethodArgumentNotValidException handling

---
+++ account-balance-service/src/main/java/it/sosinski/accountbalance/exception_handling/MethodArgumentNotValidExceptionHandler.java
@@ -0,0 +1,36 @@
+package it.sosinski.accountbalance.exception_handling;
+
+import it.sosinski.accountbalance.exception_handling.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class MethodArgumentNotValidExceptionHandler {
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public @ResponseBody ErrorResponse handler(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        return ErrorResponse.builder()
+                .message(fieldErrorsToString(fieldErrors))
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+    }
+
+    private String fieldErrorsToString(List<FieldError> fieldErrors) {
+        return fieldErrors.stream()
+                .map(err -> err.getField() + " " + err.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+    }
+}
